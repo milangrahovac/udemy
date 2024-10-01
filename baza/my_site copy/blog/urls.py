f1dc87061688d7465f319d70f57cc961c778c951
milan <grahovac85@gmail.com>
2024-10-01 20:08:19 +0200

    added dockerfile and dockercompose

--- baza/my_site copy/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("posts", views.posts, name="posts-page"),
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
-]
