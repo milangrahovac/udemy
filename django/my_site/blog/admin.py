97c8abdad0308c2474743a3707e56dd730839471
milan <grahovac85@gmail.com>
2024-10-04 21:29:55 +0200

    added sqlite

--- django/my_site/blog/admin.py
+++ django/my_site/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Author, Post, Tag
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "date",)
+    list_filter = ("author", "tags", "date",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
