62777f285c16ef438ebef1f3699cf06dceb96bb4
milan <grahovac85@gmail.com>
2024-10-27 18:11:18 +0100

    added django course site

---
+++ django/django_course_site/meetups/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("meetups/", views.index, name="index"),
+    path("meetups/<slug:meetup_slug>",
+         views.meetup_details, name="meetup-details")
+]
