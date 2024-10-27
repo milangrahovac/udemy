985ceff0775eaeccf6443f531d33518b79b3b9dc
milan <grahovac85@gmail.com>
2024-10-27 20:34:35 +0100

    added models

--- django/django_course_site/meetups/admin.py
+++ django/django_course_site/meetups/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Meetups
 
 # Register your models here.
+
+
+class PostMeetups(admin.ModelAdmin):
+    list_display = ("title", "slug", )
+    list_filter = ("title", )
+    prepopulated_fileds = {"slug": ("title",)}
+    # verbose_name = 'Meetups'
+
+
+admin.site.register(Meetups, PostMeetups)
+# admin.site.register(Meetups)
--- django/django_course_site/meetups/models.py
+++ django/django_course_site/meetups/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Meetups(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=800)
+    # location = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Meetups'
+        verbose_name_plural = 'Meetups'
+
+    def __str__(self):
+        return self.title
